160424b000f27feecaeb971c48d3198c6953fb7a
Akhilesh C <64408106+developer-Akhil@users.noreply.github.com>
2025-12-16 22:57:01 +0530

    Update and rename agentic_ai_with_custom_tools.py to custom_agentic_ai_tools.py

--- agentic_ai/custom_agentic_ai_tools.py
+++ agentic_ai/custom_agentic_ai_tools.py
@@ -1,25 +1,36 @@
 from langchain_core.tools import tool
 
 # Step 1 -- create a function
-def multiply(a,b):
+def add(a,b):
   """Adding two numbers"""
   return a+b
 
 # Step 2 -- adding data type hints
-def multiply(a: int,b: int) -> int:
+def add(a: int,b: int) -> int:
   """Multiply two numbers"""
   return a+b
 
 # Step 3 -- adding tool decorator
 @tool
-def multiply(a: int,b: int) -> int:
+def add(a: int,b: int) -> int:
   """This tool takes 2 integers and add them"""
   return a+b
 
 
-def multiply1(a,b):
+def add1(a,b):
   """Adding two numbers"""
   return a*b
 
+
+result = add.invoke({"a":5,"b":4})
+
+print(result)
+
+print(add.name)
+print(add.description)
+print(add.args)
+
+
+print(multiply.args_schema.model_json_schema())
 
 
