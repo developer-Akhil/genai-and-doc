f7a022e3af066683192a7d3501bab75af9ae9cfc
Akhilesh C <64408106+developer-Akhil@users.noreply.github.com>
2025-12-17 17:53:12 +0530

    Create bind_toolkit_agentic_ai.py

---
+++ agentic_ai/bind_toolkit_agentic_ai.py
@@ -0,0 +1,60 @@
+
+'''
+In LangChain, bind_tools (or tool binding) is a way to connect custom tools functions, APIs, or utilities — to an LLM so that the model can call them 
+directly during a conversation or reasoning process.
+'''
+
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage
+import requests
+
+
+@tool
+def add(a: int, b:int) -> int:
+  """Given 2 numbers a and b this tool returns their sum"""
+  return a*b
+
+llm = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="sk-or-v1-e1b264341b9546a557fb085cdsd8d45bd5f8dsdf502ecc4355630040282b41a12d2d7",
+    model_name="deepseek/deepseek-chat"
+)
+
+
+#print(llm.invoke('hello'))
+
+'''
+Binding a custom tool with llm.
+'''
+
+llm_with_tools = llm.bind_tools([add])
+
+#print(llm_with_tools.invoke('hello, how are you?').content)
+
+# llm_with_tools.invoke('can you sum 8 and 9?').content
+
+'''
+Tool message  
+'''
+result = llm_with_tools.invoke('can you sum 8 and 9?')
+
+
+tool_result = multiply.invoke(result.tool_calls[0])
+
+# print(tool_result)
+
+'''
+Human message  
+'''
+query = HumanMessage("can you multiply 3 with 10")
+
+messages = [query]
+
+messages.append(tool_result)
+
+messages.append(result)
+
+rs = llm_with_tools.invoke(messages).content
+
+print(rs)
